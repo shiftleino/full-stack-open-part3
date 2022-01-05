45ff78f88890f2ed3e9cbfed0652a73b81d342ec
shiftleino <>
2022-01-05 17:48:47 +0200

    Added frontend

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ morgan.token('content', (req, res) => { return JSON.stringify(req.body)})
 app.use(express.json())
 app.use(morgan(":method :url :status :res[content-length] - :response-time ms :content"))
 app.use(cors())
+app.use(express.static('build'))
 
 let persons = [
     {
