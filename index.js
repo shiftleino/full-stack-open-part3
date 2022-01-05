3476f3b84cb2f63563fd473810308d859bad51b0
shiftleino <>
2022-01-05 13:06:12 +0200

    3.4

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-const { response } = require('express')
 const express = require('express')
 const app = express()
 
@@ -45,6 +44,12 @@ app.get("/api/persons/:id", (req, res) => {
     } else {
         res.status(404).end()
     }
+})
+
+app.delete("/api/persons/:id", (req, res) => {
+    const id = Number(req.params.id)
+    persons = persons.filter(person => person.id !== id)
+    res.status(204).end()
 })
 
 const PORT = 3001
