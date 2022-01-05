3c07143903d69640c5d144e6a7963da71b4ef20c
shiftleino <>
2022-01-05 13:20:00 +0200

    3.5

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const app = express()
 
+app.use(express.json())
+
 let persons = [
     {
         id: 1,
@@ -50,6 +52,12 @@ app.delete("/api/persons/:id", (req, res) => {
     const id = Number(req.params.id)
     persons = persons.filter(person => person.id !== id)
     res.status(204).end()
+})
+
+app.post("/api/persons", (req, res) => {
+    const id = parseInt(Math.random()*10000)
+    const person = {id: id, ...req.body}
+    res.json(person)
 })
 
 const PORT = 3001
