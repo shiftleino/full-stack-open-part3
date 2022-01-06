a447d42821949b0a018c48ffcdc8b62566215817
shiftleino <>
2022-01-06 13:53:52 +0200

    3.12-3.18

--- index.js
+++ index.js
@@ -7,44 +7,45 @@ const Person = require('./models/person')
 morgan.token('content', (req, res) => { return JSON.stringify(req.body)})
 
 // Middlewares
+app.use(express.static('build'))
 app.use(express.json())
 app.use(morgan(":method :url :status :res[content-length] - :response-time ms :content"))
 app.use(cors())
-app.use(express.static('build'))
 
-app.get("/api/persons", (req, res) => {
+app.get("/api/persons", (req, res, next) => {
     Person.find({}).then(people => {
         res.json(people)
     })
+    .catch(error => next(error))
 })
 
-/*
-app.get("/info", (req, res) => {
-    const amount = people.length
-    const time = new Date()
-    const body = `<p>Phonebook has info for ${amount} people</p>
-    <p>${time}</p>`
-    res.send(body)
+app.get("/info", (req, res, next) => {
+    Person.find({}).then(people => {
+        const amount = people.length
+        const time = new Date()
+        const body = `<p>Phonebook has info for ${amount} people</p>
+        <p>${time}</p>`
+        res.send(body)
+    })
+    .catch(error => next(error))
 })
 
-app.get("/api/persons/:id", (req, res) => {
-    const id = Number(req.params.id)
-    const person = people.find(person => person.id === id)
-    if (person) {
+app.get("/api/persons/:id", (req, res, next) => {
+    Person.findById(req.params.id).then(person => {
         res.json(person)
-    } else {
-        res.status(404).end()
-    }
+    })
+    .catch(error => next(error))
 })
 
-app.delete("/api/persons/:id", (req, res) => {
-    const id = Number(req.params.id)
-    people = people.filter(person => person.id !==id)
-    res.status(204).end()
+app.delete("/api/persons/:id", (req, res, next) => {
+    Person.findByIdAndRemove(req.params.id)
+    .then(result => {
+        res.status(204).end()
+    })
+    .catch(error => next(error))
 })
-*/
 
-app.post("/api/persons", (req, res) => {
+app.post("/api/persons", (req, res, next) => {
     const body = req.body
 
     if (!body.name) {
@@ -67,6 +68,22 @@ app.post("/api/persons", (req, res) => {
     person.save().then(savedPerson => {
         res.json(savedPerson)
     })
+    .catch(error => next(error))
+})
+
+app.put("api/persons/:id", (req, res, next) => {
+    const body = req.body
+
+    const person = {
+        name: body.name,
+        number: body.number,
+    }
+
+    Person.findByIdAndUpdate(req.params.id, person, {new: true})
+    .then(updatedPerson => {
+        res.json(updatedPerson)
+    })
+    .catch(error => next(error))
 })
 
 const PORT = process.env.PORT
